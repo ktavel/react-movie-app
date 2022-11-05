de2712d49016a6daafb824151630c450070c604c
Kassidy Tavel <tavelkassidy@gmail.com>
2022-11-05 11:17:11 -0500

    updated some files

--- app.js
+++ app.js
@@ -3,34 +3,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './src/components/MovieList';
 import MovieListHeading from './src/components/MovieListHeading';
+import SearchBox from './src/components/SearchBox';
+import AddFavorite from './src/components/AddFavorites';
+
 
 const App = ()=> {
     const [movies, setMovies] = useState([]);
     const [searchValue, setSearchValue] = useState('');
 
         const getMovieRequest = async () => {
-            const url = 'https://omdbapi.com/?s=star+wars&apikey=c6ec6f7e'
+            const url = `https://omdbapi.com/?s=${searchValue}&apikey=c6ec6f7e`
 
             const response = await fetch(url);
             const responseJson = await response.json();
 
-            console.log(responseJson);
+           if (responseJson.Search) {
             setMovies(response.json.Search);
+           }
+            
         }
 
         useEffect(() => {
-            getMovieRequest();
-        }, [])
+            getMovieRequest(searchValue);
+        }, [searchValue]);
 
     
     
     return (
          <div className='container-fluid movie-app'>
-            <div className='row'>
-            <MovieListHeading />
+            <div className='row d-flex align-itmes-center mt-4 mb-4'>
+                 <MovieListHeading heading='movies' />
+                 <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
             </div>
             <div className='row'>
-            <MovieList movies={movies} />
+                <MovieList movies={movies} favoriteComponent = {AddFavorite} />
             </div>
         
     </div>
